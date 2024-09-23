17e9984c265b332abd39e7385c125369ce49200c
lai2-24 <lai2-24@itismeucci.com>
2024-09-23 09:26:08 +0200

    secondo-commit

--- src/main/java/es2_graziani/Main.java
+++ src/main/java/es2_graziani/Main.java
@@ -2,6 +2,6 @@ package es2_graziani;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Ciao Mondo!");
     }
 }
